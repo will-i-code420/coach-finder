468f1534d750473d18bd7e25111fdd21e2981129
Will Aronow <waronow123@gmail.com>
2022-02-03 14:26:14 -0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,49 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/coaches',
+		name: 'Coaches',
+		component: null
+	},
+	{
+		path: '/coaches/:id',
+		component: null,
+		children: [ { path: 'contact', component: null } ]
+	},
+	{
+		path: '/register',
+		name: 'Register',
+		component: null
+	},
+	{
+		path: '/requests',
+		name: 'Requests',
+		component: null
+	},
+	{
+		path: '/about',
+		name: 'About',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	},
+	{
+		path: '/:notFound(.*)',
+		component: null
+	}
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(process.env.BASE_URL),
+	routes
+});
 
-export default router
+export default router;
